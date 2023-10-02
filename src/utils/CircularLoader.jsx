d9a6e41329cf18a46bed3c0c3ddaff909073ab58
D Regis <irumvaregisdmc@gmail.com>
2023-10-02 16:20:35 +0200

    Circular progress

--- src/utils/CircularLoader.jsx
+++ src/utils/CircularLoader.jsx
@@ -2,24 +2,46 @@
 import React, { useEffect, useRef } from "react";
 
 const CircularLoader = ({ percentage, text }) => {
-   const circleRef = useRef(null);
-   const circumference = 2 * Math.PI * 45;
-   const strokeDashoffset = (circumference * percentage) / 100;
+  const containerRef = useRef(null);
+  const circleRef = useRef(null);
+  const circumference = 2 * Math.PI * 45;
+  const strokeDashoffset = (circumference * percentage) / 100;
 
-   useEffect(() => {
-     if (circleRef.current) {
-       // Animation from 0 to current percentage
-       circleRef.current.style.transition = "none"; // Remove transition temporarily
-       circleRef.current.style.strokeDashoffset = `${circumference}`;
-       setTimeout(() => {
-         circleRef.current.style.transition = "stroke-dashoffset 1s ease-out"; // Re-add transition
-         circleRef.current.style.strokeDashoffset = `${-strokeDashoffset}`;
-       }, 10); // Small delay to trigger the transition
-     }
-   }, [percentage]);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Animation from 0 to current percentage when entering the viewport
+          circleRef.current.style.transition = "none"; // Remove transition temporarily
+          circleRef.current.style.strokeDashoffset = `${circumference}`;
+          setTimeout(() => {
+            circleRef.current.style.transition =
+              "stroke-dashoffset 1s ease-out"; // Re-add transition
+            circleRef.current.style.strokeDashoffset = `${-strokeDashoffset}`;
+          }, 10); // Small delay to trigger the transition
+        } else {
+          // Reset the animation state when leaving the viewport
+          circleRef.current.style.transition = "none";
+          circleRef.current.style.strokeDashoffset = `${circumference}`;
+        }
+      });
+    });
 
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, [percentage]);
   return (
-    <div className="w-[100%] lg:w-[25%] px-10 flex flex-col sm:flex-row lg:flex-col gap-5 lg:gap-0 items-center">
+    <div
+      ref={containerRef}
+      className="w-[100%] lg:w-[25%] px-10 flex flex-col sm:flex-row lg:flex-col gap-5 lg:gap-0 items-center"
+    >
       <div className="relative w-32 h-32">
         <svg width="100%" height="100%" viewBox="0 0 100 100">
           <defs>
@@ -52,11 +74,12 @@ const CircularLoader = ({ percentage, text }) => {
             strokeWidth="2.5"
             strokeLinecap="round"
             strokeDasharray={circumference}
-            strokeDashoffset={-strokeDashoffset}
             transform="rotate(-90, 50, 50)" // Rotate the circle to start from the top
             style={{
               transition: "stroke-dashoffset 1s ease-out",
-            }} // Rotate the circle to start from the top
+              animationTimingFunction: "linear",
+              animationIterationCount: "infinite",
+            }}
           />
         </svg>
         <div className="absolute inset-0 flex items-center gradient-font justify-center font-bold text-[24px]">
@@ -67,6 +90,18 @@ const CircularLoader = ({ percentage, text }) => {
       <div className="flex text-gray-400 items-center text-[18px] font-[500] justify-center text-center">
         <h1>{text}</h1>
       </div>
+      <style>
+        {`
+          @keyframes progress {
+            0% {
+              stroke-dashoffset: ${circumference};
+            }
+            100% {
+              stroke-dashoffset: 0;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
